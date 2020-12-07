a859b21337c4efb0a86e42465b458b0bbc8ad338
vivek patel <vivek.170410107085@gmail.com>
2020-12-07 20:25:33 +0530

    Typo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import {Container} from 'react-bootstrap'
-import {BrowserRouter as Router,Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 
@@ -11,8 +11,8 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/product/:id' component={ProductScreen}/>
-          <Route exact path='/' component={HomeScreen}/>
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route exact path='/' component={HomeScreen} />
         </Container>
       </main>
       <Footer />
