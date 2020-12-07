ac4cc091c330d18b196be206c67dc7a911f704a2
vivek patel <vivek.170410107085@gmail.com>
2020-12-07 17:37:34 +0530

    Add products

---
+++ frontend/src/products.js
@@ -0,0 +1,44 @@
+const products = [
+  {
+    name: 'Daal',
+    image: '/images/Daal.jpg',
+    description: 'Daal',
+    category: 'Product',
+    price: 90.0,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: 'Rise',
+    image: '/images/Rise.jpg',
+    description: 'Rise',
+    category: 'Rise',
+    price: 100.0,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'Turmeric',
+    image: '/images/Turmeric.jpg',
+    description: 'Turmeric',
+    category: 'Turmeric',
+    price: 30.0,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    name: 'Wheat',
+    image: '/images/Wheat.jpg',
+    description: 'Wheat',
+    category: 'Wheat',
+    price: 120.0,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+]
+
+export default products
