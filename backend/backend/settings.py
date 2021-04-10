9cb0f87f56efa19ed66e5cfbf782a7de585a442f
vivek patel <vivek.170410107085@gmail.com>
2021-04-10 22:37:03 +0530

    staric files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,4 +125,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_URL = 'images/'
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+
 CORS_ALLOW_ALL_ORIGINS = True
